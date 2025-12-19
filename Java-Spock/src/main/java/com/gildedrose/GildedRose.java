dde3545de1a12833b12d3f1871733bc3f399d591
Alexander Kracht <deralex1985@gmail.com>
2025-12-19 16:24:14 +0100

    Remove BackstagePass from AgedBrie handling

--- Java-Spock/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Spock/src/main/java/com/gildedrose/GildedRose.java
@@ -33,20 +33,6 @@ class GildedRose {
         if (item.name.equals(AGED_BRIE.description)) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
-
-                if (item.name.equals(BACKSTAGE_PASSES.description)) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
-                }
             }
             item.sellIn = item.sellIn - 1;
         }
